3667d31470aaed71e1dfd0861b7d40757b438715
SMS-D01\JVFMatos <jvmatosfagundes@gmail.com>
2024-05-07 10:41:09 -0300

    Construção do modulos de usuarios

--- nestjs-clean-arch/src/shared/infrastructure/env-config/env-config.module.ts
+++ nestjs-clean-arch/src/shared/infrastructure/env-config/env-config.module.ts
@@ -4,7 +4,15 @@ import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
 import { join } from 'path'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
+      ],
+    }),
+  ],
   providers: [EnvConfigService],
+  exports: [EnvConfigService],
 })
 export class EnvConfigModule extends ConfigModule {
   static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
